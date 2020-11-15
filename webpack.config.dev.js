4a2e490442b5bb433fdb74119f881ffd1f12c527
Stanislav Mashyn <sammusicdnb@gmail.com>
2020-11-15 18:15:42 +0200

    fix dev run

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,6 +1,13 @@
+const path = require('path');
 const webpackConfCommon = require('./webpack/webpack.common');
 
 module.exports = Object.assign(webpackConfCommon, {
   watch: true,
-  mode: 'development'
+  mode: 'development',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    historyApiFallback: true,
+    compress: true,
+    port: 3000
+  }
 });

d5318562588275d16650000df365f44211f87c58
Stanislav Mashyn <sammusicdnb@gmail.com>
2020-11-19 19:09:44 +0200

    fix header links

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
 import pkg from '../../package.json';
 
+const PATH = window.location.hostname === 'localhost' ? '' : `/products-calc`;
 export function Header(){
   return (
     <Navbar bg="light" expand="lg">
@@ -9,8 +10,8 @@ export function Header(){
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse className="justify-content-end">
         <Nav className="mr-auto">
-          <Nav.Link href="/">Calc</Nav.Link>
-          <Nav.Link href="/list">Product List</Nav.Link>
+          <Nav.Link href={`${PATH}/`}>Calc</Nav.Link>
+          <Nav.Link href={`${PATH}/list`}>Product List</Nav.Link>
         </Nav>
         <Navbar.Text>
           v. {pkg.version}
